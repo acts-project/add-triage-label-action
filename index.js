d20a591715a6e6a6f31d35a2e17e95d112e9d6a7
Paul Gessinger <hello@paulgessinger.com>
2020-03-27 11:10:37 +0100

    update

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ try {
   const labels = core.getInput('labels').split(",");
   console.log(`Eligible labels are ${labels}`);
 
+  const triage = core.getInput("triage_label");
+  console.log(`Triage label is ${triage}`);
+
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  const action = github.context.payload.action;
+  console.log(`Action is ${action}`);
+
+  if(github.context.payload.issue !== undefined) {
+    console.log("This is an issue");
+  }
+  else if(github.context.payload.pull_request !== undefined) {
+    console.log("This is an issue");
+  }
+  else {
+    core.setFailed("Invalid event combination");
+  }
+  
+
+
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
